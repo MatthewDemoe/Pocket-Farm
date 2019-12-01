2e6f4eacad6cd82de819ab10b6ecf1f06b223fe9
Matthew <mattdemoe@gmail.com>
2019-12-01 13:53:28 -0500

    plants grow but harvesting logic isn't implemented yet

--- lib/FarmPlot.dart
+++ lib/FarmPlot.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:pocket_farm/Enums.dart';
 
 import 'Plants.dart';
 class FarmPlot{
   Plant plant;
+  DateTime timeHalfCompleted;
   DateTime timeCompleted; 
+
+  GestureDetector gestureDetector;
+
+  FarmPlot({this.gestureDetector});
 
   bool plantSomething(SeedType seed)
   {
@@ -14,16 +20,19 @@ class FarmPlot{
     {
       case SeedType.carrot:
       plant = new Carrot();
+      timeHalfCompleted = new DateTime.now().add(new Duration(minutes: (plant.minutesToGrow ~/ 2)));
       timeCompleted = new DateTime.now().add(new Duration(minutes: plant.minutesToGrow));
       break;
 
       case SeedType.cabbage:
       plant = new Cabbage();
+      timeHalfCompleted = new DateTime.now().add(new Duration(minutes: (plant.minutesToGrow ~/ 2)));
       timeCompleted = new DateTime.now().add(new Duration(minutes: plant.minutesToGrow));
       break;
 
       case SeedType.kale:
       plant = new Kale();
+      timeHalfCompleted = new DateTime.now().add(new Duration(minutes: (plant.minutesToGrow ~/ 2)));
       timeCompleted = new DateTime.now().add(new Duration(minutes: plant.minutesToGrow));
       break;
     }
@@ -45,7 +54,18 @@ class FarmPlot{
     {
       if(DateTime.now().isAfter(timeCompleted))
         return true;
-    }
+    }  
+
+    return false;
+  }
+
+  bool isSprouted()
+  {
+    if(isPlanted())
+    {
+      if(DateTime.now().isAfter(timeHalfCompleted))
+        return true;
+    }  
 
     return false;
   }
