f4012a01b7867ca671c654fd78ccc937dc7e5093
Matthew <mattdemoe@gmail.com>
2019-11-30 21:51:39 -0500

    Added localization. A few game classes. Initial game loop etc.

--- lib/FarmScreen.dart
+++ lib/FarmScreen.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'Enums.dart';
+
 
 class FarmScreen extends StatefulWidget {
   FarmScreen({Key key, this.title}) : super(key: key);
@@ -9,10 +16,69 @@ class FarmScreen extends StatefulWidget {
   _FarmScreen createState() => _FarmScreen();
 }
 
-class _FarmScreen extends State<FarmScreen> {
+class _FarmScreen extends State<FarmScreen> {  
   int numFields = 5;
   int counter = 0;
   List<GestureDetector> fields = new List<GestureDetector>();
+
+  @override void initState() {
+    super.initState();
+    _scheduleTick();
+  }
+
+  void _tick(Duration timestamp){
+
+    _scheduleTick();
+  }
+
+  //Taken from flame's game loop
+  void _scheduleTick(){
+    SchedulerBinding.instance.scheduleFrameCallback(_tick);
+  }
+
+  Future<void> _seedPicker() async{
+    switch (await showDialog<SeedType>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text(FlutterI18n.translate(context, "words.selectseed")),
+          children: [
+            SimpleDialogOption(
+              onPressed: (){
+                Navigator.pop(context, SeedType.carrot);
+              },
+              child: Text(FlutterI18n.translate(context, "words.carrot")),
+            ),
+            SimpleDialogOption(
+              onPressed: (){
+                Navigator.pop(context, SeedType.cabbage);
+              },
+              child: Text(FlutterI18n.translate(context, "words.cabbage")),
+            ),
+            SimpleDialogOption(
+              onPressed: (){
+                Navigator.pop(context, SeedType.kale);
+              },
+              child: Text(FlutterI18n.translate(context, "words.kale")),
+            ),
+          ]
+        );
+      }
+    )){
+      case SeedType.carrot:
+      print('carrot planted');
+      break;
+
+      case SeedType.cabbage:
+      print('cabbage planted');
+      break;
+
+      case SeedType.kale:
+      print('kale planted');
+      break;
+    }
+
+  }
 
   Column buildRows() {
     List<Row> rows = new List<Row>();
@@ -53,22 +119,28 @@ class _FarmScreen extends State<FarmScreen> {
           scale: 3.0,
           fit: BoxFit.cover,
         ),
-        onTap: () => {print('tapped land $counter'), counter++},
+        onTap: () => _seedPicker(),//() => {print('tapped land $counter'), counter++},
       ));
     }
 
     return Scaffold(
-      body: ListView(
-        children: [
-        Image.asset('assets/images/Farm.png', 
-        alignment: Alignment.topCenter, 
-        fit: BoxFit.scaleDown, 
-        scale: 0.5,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/grass.jpg'),
+            repeat: ImageRepeat.repeat,
+          ),
         ),
-
-        buildRows(),
-        
-      ]),
+        child: ListView(children: [
+          Image.asset(
+            'assets/images/Farm.png',
+            alignment: Alignment.topCenter,
+            fit: BoxFit.scaleDown,
+            scale: 0.5,
+          ),
+          buildRows(),
+        ]),
+      ),
 
       drawer: ListView(
         padding: EdgeInsets.only(top: 100.0, right: 200.0),
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+
 import 'MyHomePage.dart';
 import 'FarmScreen.dart';
 import 'ShopScreen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pocket Farm',
+      localizationsDelegates: [
+        FlutterI18nDelegate(
+          useCountryCode: false,
+          fallbackFile: 'fr',
+          path: 'assets/i18n',
+        ),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      title: "Pocket Farm",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Countryside',
       ),
-      //home: MyHomePage(title: 'Pocket Farm'),
 
       initialRoute: '/',
       routes: {
